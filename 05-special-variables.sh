9e87f97b2a3771a613679e39a36ff822a7611c48
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-04-24 16:22:04 +0530

    committing arrays

--- 05-special-variables.sh
+++ 05-special-variables.sh
@@ -3,4 +3,4 @@ echo "Enter few arguments:"
 echo "all variables are: $@"
 echo "No. of variables passed: $#"
 echo "CUrrent file name is : $0"
-echo "Previously used command is : $?
+echo "Previously used command is : $?"
