8ed00fe464fb8bfefdf2127e7e47c9092e79631b
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-04-24 16:06:08 +0530

    committing arrays

--- variables.sh
+++ variables.sh
@@ -3,6 +3,6 @@
 PERSON1=$1 #no space between variable and value
 PERSON2=$2
 
-echo "Hello $PERSON1, how are you?"
-echo "I'm good. How are you $PERSON2?"
+echo "Hello ${PERSON1}, how are you?"
+echo "I'm good. How are you ${PERSON2}?"
 echo "I'm good too. Thanks $PERSON1.."
