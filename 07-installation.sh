92c0763eff16206edea1a4dfa3d0b4f2104c2d79
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-04-24 17:12:53 +0530

    committing arrays

--- 07-installation.sh
+++ 07-installation.sh
@@ -5,7 +5,9 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Please run with super user access"
+    exit 1 #manually exiting the code if error comes
 else
     dnf install mysql -y
+    exit 0
 fi
 echo "Is it still running?"
