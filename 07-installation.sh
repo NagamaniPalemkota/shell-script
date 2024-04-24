a4db4679109cf9a28dc86da837836dedd5d10ddb
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-04-24 17:17:35 +0530

    committing arrays

--- 07-installation.sh
+++ 07-installation.sh
@@ -7,7 +7,15 @@ then
     echo "Please run with super user access"
     exit 1 #manually exiting the code if error comes
 else
-    dnf install mysql -y
-    exit 0
+    echo "You are super user"
 fi
+    
+    dnf install mysqlll -y
+
+    if [ $? -ne 0]
+        echo "Installation of mysql is failure"
+    else
+        echo "Installation of mysql is success"
+    fi
+
 echo "Is it still running?"
