eeb79683956c1a9ee943284f759844e1088419a5
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-04-25 10:15:59 +0530

    committing functions

---
+++ 08-functions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash/
+
+USERID=$(id -u)
+
+VALIDATE(){
+
+    if [ $1 -ne 0]
+    then
+        echo "$2is failure"
+        exit 197
+    else
+        echo "$2 is success"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run with super user access"
+    exit 1 #manually exiting the code if error comes
+else
+    echo "You are super user"
+fi
+    
+    dnf install mysql -y
+    VALIDATE $? "Installing MySQL"
+
+echo "Is it still running?"
