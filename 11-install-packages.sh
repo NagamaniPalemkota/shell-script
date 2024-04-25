3011f526d83ae3391a0c3604a042257abcb29897
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-04-25 14:18:18 +0530

    installing packages

--- 11-install-packages.sh
+++ 11-install-packages.sh
@@ -1,3 +1,17 @@
 #!/bin/bash/
 
-echo "Packages to install : $@"
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run with super user access"
+    exit 1 #manually exiting the code if error comes
+else
+    echo "You are super user"
+fi
+
+
+for i in $#
+do
+    echo "Packages to install : $i"
+done
