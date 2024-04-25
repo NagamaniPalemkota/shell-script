9800964facb34883065b38f9459878dc73511eda
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-04-25 11:18:45 +0530

    committing functions

---
+++ 09-colors.sh
@@ -0,0 +1,34 @@
+#!/bin/bash/
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo "$0" | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is $R failure $N"
+        exit 197
+    else
+        echo -e "$2 is $G success $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run with super user access"
+    exit 1 #manually exiting the code if error comes
+else
+    echo "You are super user"
+fi
+    
+    dnf install mysql -y &>> $LOGFILE
+    VALIDATE $? "Installing MySQL"
+
+echo "Is it still running?"
