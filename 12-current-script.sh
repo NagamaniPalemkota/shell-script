2f10c1b3d2ae129f016b79fa6e780a9ba8838d8d
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-04-26 23:13:19 +0530

    committing other script

--- 12-current-script.sh
+++ 12-current-script.sh
@@ -5,5 +5,5 @@ COURSE="Devops with AWS"
 echo "Course before calling other script is: ${COURSE}"
 echo "Process ID of current script is:$$"
 
-./other-script.sh
+./13-other-script.sh
 echo "Course after calling the other script is:${COURSE}"
