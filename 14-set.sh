6ad356d535f3f7cecba6cab621c51572833347da
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-04-28 19:57:15 +0530

    using common script file

---
+++ 14-set.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e #used to handle errors by shell
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run with super user access"
+    exit 1 #manually exiting the code if error comes
+else
+    echo "You are super user"
+fi
+    
+    dnf install mysqlll -y
+    dnf install git -y
+
+    if [ $? -ne 0]
+    then
+        echo "Installation of mysql is failure"
+    else
+        echo "Installation of mysql is success"
+    fi
+
+echo "Is it still running?"
