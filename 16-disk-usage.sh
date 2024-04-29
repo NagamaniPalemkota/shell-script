9c9870a34ec059bb3757f64c19beac8b94cadc9f
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-04-29 12:21:10 +0530

    scheduling old logs deletion

--- 16-disk-usage.sh
+++ 16-disk-usage.sh
@@ -3,7 +3,7 @@
 DISK_USAGE=$(df -hT|grep xfs)
 DISK_THRESHOLD=50
 
-while IFS=read -r line
+while IFS= read -r line
     do 
         DISK_USED=$(echo $line|awk -F " " 'print {6F}'| cut -d "%" -f1)
         FOLDER=$(echo $line|awk -F " " 'print {NF}')
